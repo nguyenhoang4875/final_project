02bd9843dba57e9363dcb5e9bf650d64ff5d1612
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-10-18 18:50:57 +0700

    try config for deploy to heroku

--- app/database/index.js
+++ app/database/index.js
@@ -5,17 +5,20 @@ const Mongoose = require('mongoose');
 const logger = require('../logger');
 const dotenv = require('dotenv');
 const { DB_HOST_CHAT, DB_NAME_CHAT } = process.env;
-//const mongoDB = `mongodb+srv://mariana:mariana@cluster0.h8dsg.mongodb.net/mariana?retryWrites=true&w=majority`;
+const mongoDB = `mongodb+srv://mariana:mariana@cluster0.h8dsg.mongodb.net/mariana?retryWrites=true&w=majority`;
+/*
 const dbURI = "mongodb://" +
 	encodeURIComponent(config.db.username) + ":" +
 	encodeURIComponent(config.db.password) + "@" +
 	config.db.host + ":" +
 	config.db.port + "/" +
 	config.db.name;
-
 console.log('dbURI ', dbURI);
 
-Mongoose.connect(dbURI, { useNewUrlParser: true });
+*/
+
+
+Mongoose.connect(mongoDB, { useNewUrlParser: true });
 
 // Connect to the database
 // construct the database URI and encode username and password.
--- app/socket/index.js
+++ app/socket/index.js
@@ -4,6 +4,7 @@ const config = require('../config');
 const redis = require('redis').createClient;
 const adapter = require('socket.io-redis');
 const https = require('https');
+const http = require('http');
 const fs = require('fs');
 
 const RoomService = require('../services/RoomService');
@@ -190,20 +191,21 @@ const ioEvents = function (io) {
 const init = function (app) {
 
   // set https server
-  const options = {
+/*  const options = {
     key: fs.readFileSync('./certificates/key.pem', 'utf8'),
     cert: fs.readFileSync('./certificates/cert.pem', 'utf8'),
   };
 
-  const server = https.createServer(options, app);
+  const server = https.createServer(options, app);*/
 
+  const server = http.createServer(app);
   const io = require('socket.io')(server);
   const mongoAdapter = require('socket.io-adapter-mongo');
 
   // Force Socket.io to ONLY use "websockets"; No Long Polling.
   io.set('transports', ['websocket']);
 
-  io.adapter(mongoAdapter('mongodb://localhost:27017'));
+ // io.adapter(mongoAdapter('mongodb://localhost:27017'));
 
   // Allow sockets to access session data
   io.use((socket, next) => {
